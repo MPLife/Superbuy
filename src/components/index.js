8f503118507e50225aa9b28e74df3439c551f012
ShiroPika <shiropika@gmail.com>
2016-12-04 00:49:43 +0800

    https://github.com/lzxb/vue2-demo

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by shiro on 2016-12-04.
+ */
+import header from './header'
+export default { header }
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by shiro on 2016-12-04.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import user from './user'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    strict: process.env.NODE_ENV !== 'production', //在非生产环境下，使用严格模式
+    modules: {
+        user
+    }
+})
